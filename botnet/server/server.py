104524680fba134e5a112842d18f4680f9de39c7
Shaarkzy <ibrababdulshakur2003@gmail.com>
2024-09-23 22:57:09 +0100

    Update for server

--- botnet/server/server.py
+++ botnet/server/server.py
@@ -3,7 +3,8 @@ from os import system as sys
 import socket as soc
 import time as tm
 import subprocess as sub
-
+from datetime import datetime as dt
+import threading
 
 '''
 -check last id
@@ -20,19 +21,20 @@ import subprocess as sub
 
 ''' 
 
-class botnet:
+class calculations:
     def __init__(self):
         #create a global variable for constants
         try:
             #global variable here
             self.error = 'error'
-            self.directory = sub.getoutput('pwd')
+            self.userdata = sub.getoutput('pwd')+'/users.txt'
+            self.timestamp = sub.getoutput('pwd')+'/timestamp.txt'
         except:
             pass
 
     def check_id_info(self):
         #open file 
-        open_file = open(self.directory+'/users.txt', 'r')
+        open_file = open(self.userdata, 'r')
         lines = open_file.readlines()
         open_file.close()
         #input user detail to fetch full information
@@ -47,12 +49,12 @@ class botnet:
 
     def create_id(self):
         #open file
-        open_file = open(self.directory+'/users.txt', 'r')
+        open_file = open(self.userdata, 'r')
         lines = open_file.readlines()
         open_file.close
         for line in lines:
             user_line = line.strip().split('-')[0]
-        create_file = open(self.directory+'/users.txt', 'a')
+        create_file = open(self.userdata, 'a')
         while True:
             user_spe = input('enter any 8 random letter: ')
             length = len(user_spe)
@@ -62,13 +64,15 @@ class botnet:
                 break
         new_id = int(user_line) + 10
         key_range = self.calc_range()+'\n'
-        new_data = str(new_id) + '-' + user_spe + '-' + key_range
+        new_data = str(new_id) + '-' + user_spe + '-' + key_range 
         create_file.write(new_data)
+        #initiate time stamp
+        self.create_time_stamp()
         create_file.close()
 
     def calc_range(self):
         #open file to get the last assigned key range
-        open_file = open(self.directory+'/users.txt', 'r')
+        open_file = open(self.userdata, 'r')
         lines = open_file.readlines()
         open_file.close()
         for line in lines:
@@ -79,11 +83,94 @@ class botnet:
         #create key range
         while start <= stop:
             end = start + interv
-            key_range = hex(start).lstrip('0x').zfill(12) + ':' + hex(min(end, stop +1) -1).lstrip('0x').zfill(12) 
+            key_range = hex(start).lstrip('0x').zfill(12) + ':' + hex(min(end, stop +1) -1).lstrip('0x').zfill(12)
             start = end + 1
             return key_range
 
+    def create_time_stamp(self):
+        current_datetime = dt.now()
+        formated_time_date = current_datetime.strftime('%y-%m-%d')
+        formated_time_time = current_datetime.strftime('%H:%M:%S')
 
-botnet = botnet()
-botnet.create_id()
+        #calculate timestamp
+        calculated_stamp = formated_time_date + ' ' + formated_time_time
+        #create file to store timestamp
+        open_file = open(self.timestamp, 'w')
+        open_file.write(calculated_stamp)
+        open_file.close()
+
+
+
+
+
+
+
+class server_server:
+    def __init__(self):
+        #declare global variable
+        self.timestamp =sub.getoutput('pwd')+'/timestamp.txt'
+        self.soc = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
+
+    def open_server_timestamp(self):
+        sock = self.soc
+        ip = '0.0.0.0'
+        port = 'unique port A'
+        sock.bind((ip, int(port)))
+        while True:
+            try:
+                open_file = open(self.timestamp, 'r')
+                data = open_file.read()
+
+                sock.listen(5)
+                conn, addr = sock.accept()
+                conn.send(data.encode())
+            except KeyboardInterrupt:
+                conn.close()
+                break
+
+    def connect_server_timestamp(self):
+        sock = self.soc
+        while True:
+            try:
+               ip_A_B = ['server A ip', 'server B ip']
+               port = 'server A and B port'
+               for ip in ip_A_B:
+                   try:
+                       sock.settimeout(10)
+                       sock.connect((ip, int(port)))
+                       break
+                   except (soc.error, ConnectionRefusedError):
+                      pass
+               data = sock.recv(20480).decode()
+               #compare stamps
+               mate_server_stamp = str(data)
+               open_file_home = open(self.timestamp, 'r')
+               home_server_stamp = open_file_home.read()
+               stp1 = datetime.strptime(mate_server_stamp, '%y-%m-%d %H:%M:%S')
+               stp2 = datetime.strptime(home_server_stamp, '%y-%m-%d %H:%M:%S')
+               if stp1 > stp2:
+                   open_file = open(self.timestamp, 'w')
+                   open_file.write(str(data))
+                   open_file.close()
+               else:
+                   pass
+            except KeyboardInterrupt:
+                sock.close()
+
+
+
+
+
+
+
+       
+#code wont be executed, not a working code !!!!!!!
+
+calculations, server_server = calculations(), server_server()
+engine1 = threading.Thread(target=calculations.create_id)
+engine2 = threading.Thread(target=server_server.open_server_timestamp)
+engine3 = threading.Thread(target=server_server.connect_server_timestamp)
+
+
+
 
